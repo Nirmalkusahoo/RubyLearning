0b7054230f7ab4646c4894d2628abe64e57f5e00
Nirmal Kumar Sahoo <nirmal.kumar.sahoo@sap.com>
2020-06-21 14:05:32 +0530

    initial commit

---
+++ block.rb
@@ -0,0 +1,14 @@
+puts "THis is my test case"
+BEGIN {
+    puts "This is my start of code, inside begin block"
+}
+puts "This is after BEGIN block"
+
+
+
+END{
+  puts "Second END block"
+}
+END{
+  puts "END block"
+}
---
+++ first_rubby.rb
@@ -0,0 +1,23 @@
+print "Just Started with Ruby"
+puts "------Second statement  in same Line"
+puts "New Line"
+
+# comment
+# Mutiple Comment Syntax
+=begin
+    Multiline comment
+    second comment
+=end
+
+#Take Input from user
+puts "Enter Your name"
+name = gets
+
+puts "Awsome :)" +name
+
+# First task
+puts "Enter First Name"
+fn= gets
+puts "Enter Last Anme"
+ln = gets
+print  "Welcome "+fn +ln
